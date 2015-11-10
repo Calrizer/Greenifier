a2626f5a0a4ae356797bd8442347f41d5f6954e1
DevCraftProductions <DevCraftProductions@users.noreply.github.com>
2015-11-10 08:35:49 -0600

    Added Core

--- src/Greenifier/GreenifierMain.php
+++ src/Greenifier/GreenifierMain.php
@@ -10,6 +10,10 @@ use pocketmine\level\format\mcregion\Chunk__32bit.php
 use pocketmine\level\format\mcregion\Chunk__64bit.php
 {
   
-  
+  for($x = -10000; $x <= 10000; $x++) {
+  for($z = -10000; $z <= 10000; $z++) {
+    $level->setBiomeColor($x,$z,127,169,76);
+  }
+}
   
 }
